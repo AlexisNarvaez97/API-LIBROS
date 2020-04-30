1a4639d6aaa20c28aee731a5b0696957db6e3486
AlexisNarvaez <alexisnarvaez97@hotmail.com>
2020-04-30 11:57:47 -0500

    Refactorizando con merge-graphql-schemas, añandiendo al index config

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,8 +1,11 @@
-import { GraphQLSchema } from 'graphql';
 import 'graphql-import-node';
-import typeDefs from './schema.graphql';
+import { GraphQLSchema } from 'graphql';
 import resolvers from './../resolvers/resolversMap';
 import { makeExecutableSchema } from 'graphql-tools';
+
+import { fileLoader, mergeTypes } from 'merge-graphql-schemas';
+
+const typeDefs = mergeTypes(fileLoader(`${__dirname}/**/*.graphql` ), {all: true});
 
 const schema: GraphQLSchema = makeExecutableSchema({
     typeDefs,
